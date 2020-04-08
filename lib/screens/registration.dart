781515325efa9da2ef42d2fa2620e88dd7dcc476
juditlehoczki <lehoczki.judit@gmail.com>
2020-04-08 17:06:06 +0100

    start off with registration, passed data down from Home to Registration

--- lib/screens/registration.dart
+++ lib/screens/registration.dart
@@ -1,13 +1,38 @@
+import 'dart:core';
 import 'package:flutter/material.dart';
 
 class Registration extends StatefulWidget {
+  final String screen;
+
+  Registration({Key key, @required this.screen}) : super(key: key);
   @override
   _RegistrationState createState() => _RegistrationState();
 }
 
 class _RegistrationState extends State<Registration> {
+  // final String screen;
+
+  // if (screen == "volunteer") {}
+  // ;
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("Registration Page"));
+    if (widget.screen == "volunteer") {
+      print("hello");
+    }
+    return Container();
   }
 }
+
+// class Registration extends StatefulWidget {
+//   @override
+//   _RegistrationState createState() => _RegistrationState();
+// }
+
+// class _RegistrationState extends State<Registration> {
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(child: Text("Registration Page"));
+//   }
+// }
