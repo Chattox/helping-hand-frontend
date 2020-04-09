9a500e67ac28b04c51aefc947fc35eabcf2aa8b1
juditlehoczki <lehoczki.judit@gmail.com>
2020-04-09 15:05:26 +0100

    make registration scrollable and hide password when entered

--- lib/screens/registration.dart
+++ lib/screens/registration.dart
@@ -23,7 +23,7 @@ class _RegistrationState extends State<Registration> {
   String enteredName = '';
   String enteredEmail = '';
   String enteredPassword = '';
-  int enteredNumber = 0;
+  int enteredPhoneNumber = 0;
   String enteredPostcode = '';
   String enteredStreetAddress = '';
   String enteredCity = "";
@@ -38,182 +38,193 @@ class _RegistrationState extends State<Registration> {
         padding: EdgeInsets.all(10.0),
         child: Form(
           key: _formKey,
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(top: 10.0, bottom: 7.0, right: 15.0),
-                child: TextFormField(
-                  controller: nameController,
-                  decoration: InputDecoration(
-                    labelText: 'First Name',
-                    icon: Icon(Icons.person,
-                        color: Theme.of(context).primaryColor),
-                  ),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please enter your first name';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 7.0, bottom: 7.0, right: 20.0),
-                child: TextFormField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    labelText: 'Email Address',
-                    icon: Icon(Icons.alternate_email,
-                        color: Theme.of(context).primaryColor),
-                  ),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please enter your email address';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 7.0, bottom: 7.0, right: 20.0),
-                child: TextFormField(
-                  controller: passwordController,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    icon: Icon(Icons.vpn_key,
-                        color: Theme.of(context).primaryColor),
-                  ),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 7.0, bottom: 7.0, right: 20.0),
-                child: TextFormField(
-                  controller: phoneNumberController,
-                  keyboardType: TextInputType.number,
-                  inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
-                  decoration: InputDecoration(
-                    labelText: 'Telephone Number',
-                    icon:
-                        Icon(Icons.call, color: Theme.of(context).primaryColor),
-                  ),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please enter your telephone number';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 7.0, bottom: 7.0, right: 20.0),
-                child: TextFormField(
-                  controller: postcodeController,
-                  decoration: InputDecoration(
-                    labelText: 'Postcode',
-                    icon: Icon(Icons.location_on,
-                        color: Theme.of(context).primaryColor),
-                  ),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please enter your postcode';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              if (widget.screen == 'helpee')
+          child: SingleChildScrollView(
+            child: (Column(
+              children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.only(top: 7.0, bottom: 7.0, right: 20.0),
+                  padding: EdgeInsets.only(top: 10.0, bottom: 7.0, right: 15.0),
                   child: TextFormField(
-                    controller: streetAddressController,
+                    controller: nameController,
                     decoration: InputDecoration(
-                      labelText: 'Street Address',
-                      icon: Icon(Icons.location_on,
+                      labelText: 'First Name',
+                      icon: Icon(Icons.person,
                           color: Theme.of(context).primaryColor),
                     ),
                     validator: (value) {
                       if (value.isEmpty) {
-                        return 'Please enter your street address';
+                        return 'Please enter your first name';
                       }
                       return null;
                     },
                   ),
                 ),
-              if (widget.screen == 'helpee')
                 Padding(
                   padding: EdgeInsets.only(top: 7.0, bottom: 7.0, right: 20.0),
                   child: TextFormField(
-                    controller: cityController,
+                    controller: emailController,
                     decoration: InputDecoration(
-                      labelText: 'City',
-                      icon: Icon(Icons.location_on,
+                      labelText: 'Email Address',
+                      icon: Icon(Icons.alternate_email,
                           color: Theme.of(context).primaryColor),
                     ),
                     validator: (value) {
                       if (value.isEmpty) {
-                        return 'Please enter your city';
+                        return 'Please enter your email address';
                       }
                       return null;
                     },
                   ),
                 ),
-              if (widget.screen == 'volunteer')
                 Padding(
-                  padding: EdgeInsets.only(top: 15.0),
-                  child: Row(
-                    children: <Widget>[
-                      Text("0"),
-                      Expanded(
-                        child: Slider(
-                          value: enteredDistanceToTravel,
-                          min: 0,
-                          max: 10,
-                          divisions: 20,
-                          label: "$enteredDistanceToTravel",
-                          activeColor: Theme.of(context).primaryColor,
-                          onChanged: (value) {
-                            setState(() {
-                              enteredDistanceToTravel = value;
-                            });
-                          },
-                        ),
-                      ),
-                      Text("10"),
+                  padding: EdgeInsets.only(top: 7.0, bottom: 7.0, right: 20.0),
+                  child: TextFormField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      icon: Icon(Icons.vpn_key,
+                          color: Theme.of(context).primaryColor),
+                    ),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 7.0, bottom: 7.0, right: 20.0),
+                  child: TextFormField(
+                    controller: phoneNumberController,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      WhitelistingTextInputFormatter.digitsOnly
                     ],
+                    decoration: InputDecoration(
+                      labelText: 'Telephone Number',
+                      icon: Icon(Icons.call,
+                          color: Theme.of(context).primaryColor),
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter your telephone number';
+                      }
+                      return null;
+                    },
                   ),
                 ),
-              if (widget.screen == 'volunteer')
                 Padding(
-                  padding: EdgeInsets.only(bottom: 15.0),
+                  padding: EdgeInsets.only(top: 7.0, bottom: 7.0, right: 20.0),
+                  child: TextFormField(
+                    controller: postcodeController,
+                    decoration: InputDecoration(
+                      labelText: 'Postcode',
+                      icon: Icon(Icons.location_on,
+                          color: Theme.of(context).primaryColor),
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter your postcode';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                if (widget.screen == 'helpee')
+                  Padding(
+                    padding:
+                        EdgeInsets.only(top: 7.0, bottom: 7.0, right: 20.0),
+                    child: TextFormField(
+                      controller: streetAddressController,
+                      decoration: InputDecoration(
+                        labelText: 'Street Address',
+                        icon: Icon(Icons.location_on,
+                            color: Theme.of(context).primaryColor),
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter your street address';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                if (widget.screen == 'helpee')
+                  Padding(
+                    padding:
+                        EdgeInsets.only(top: 7.0, bottom: 7.0, right: 20.0),
+                    child: TextFormField(
+                      controller: cityController,
+                      decoration: InputDecoration(
+                        labelText: 'City',
+                        icon: Icon(Icons.location_on,
+                            color: Theme.of(context).primaryColor),
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter your city';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                if (widget.screen == 'volunteer')
+                  Padding(
+                    padding: EdgeInsets.only(top: 15.0),
+                    child: Row(
+                      children: <Widget>[
+                        Text("0"),
+                        Expanded(
+                          child: Slider(
+                            value: enteredDistanceToTravel,
+                            min: 0,
+                            max: 10,
+                            divisions: 20,
+                            label: "$enteredDistanceToTravel",
+                            activeColor: Theme.of(context).primaryColor,
+                            onChanged: (value) {
+                              setState(() {
+                                enteredDistanceToTravel = value;
+                              });
+                            },
+                          ),
+                        ),
+                        Text("10"),
+                      ],
+                    ),
+                  ),
+                if (widget.screen == 'volunteer')
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 15.0),
+                    child: Text(
+                        "Distance willing to travel: $enteredDistanceToTravel miles"),
+                  ),
+                RaisedButton(
+                  color: Theme.of(context).primaryColor,
                   child: Text(
-                      "Distance willing to travel: $enteredDistanceToTravel miles"),
-                ),
-              RaisedButton(
-                color: Theme.of(context).primaryColor,
-                child: Text(
-                  "Register",
-                  textScaleFactor: 1.2,
-                ),
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    setState(() {
-                      enteredName = nameController.text;
-                      enteredEmail = emailController.text;
-                      enteredPassword = passwordController.text;
-                      enteredNumber = int.parse(phoneNumberController.text);
-                      enteredPostcode = postcodeController.text;
-                      reset();
-                    });
-                  }
-                },
-              )
-            ],
+                    "Register",
+                    textScaleFactor: 1.2,
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      setState(() {
+                        enteredName = nameController.text;
+                        enteredEmail = emailController.text;
+                        enteredPassword = passwordController.text;
+                        enteredPhoneNumber =
+                            int.parse(phoneNumberController.text);
+                        enteredPostcode = postcodeController.text;
+                        enteredStreetAddress = streetAddressController.text;
+                        enteredCity = cityController.text;
+
+                        reset();
+                      });
+                    }
+                  },
+                )
+              ],
+            )),
           ),
         ),
       ),
@@ -225,24 +236,37 @@ class _RegistrationState extends State<Registration> {
     emailController.text = "";
     passwordController.text = "";
     phoneNumberController.text = "";
+    postcodeController.text = "";
+    streetAddressController.text = "";
+    cityController.text = "";
+
+    print(enteredName);
+    print(enteredEmail);
+    print(enteredPassword);
+    print(enteredPhoneNumber);
+    print(enteredPostcode);
+    print(enteredStreetAddress);
+    print(enteredCity);
+    print(enteredDistanceToTravel);
+
     setState(() {
       enteredName = "";
       enteredEmail = "";
       enteredPassword = "";
-      enteredNumber = 0;
+      enteredPhoneNumber = 0;
+      enteredPostcode = "";
+      enteredStreetAddress = "";
+      enteredCity = "";
+      enteredDistanceToTravel = 5.0;
     });
+
+    print(enteredName);
+    print(enteredEmail);
+    print(enteredPassword);
+    print(enteredPhoneNumber);
+    print(enteredPostcode);
+    print(enteredStreetAddress);
+    print(enteredCity);
+    print(enteredDistanceToTravel);
   }
 }
-
-// class Registration extends StatefulWidget {
-//   @override
-//   _RegistrationState createState() => _RegistrationState();
-// }
-
-// class _RegistrationState extends State<Registration> {
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(child: Text("Registration Page"));
-//   }
-// }
